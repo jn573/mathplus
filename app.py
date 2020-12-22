4ced74f058c6fe19bce1d6c194fd027553d507ea
James <jn573@users.noreply.github.com>
2020-12-22 20:08:10 +0200

    Testing deploy HQ

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/welcome')
+def welcome():
+    msg = 'foobar'
+    t = 'python flask'
+    render_template('welcome.html', title=t, message=msg)
 
 
 if __name__ == '__main__':
