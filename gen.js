287793680c482e12fccba2298511a581516dbae0
James <jn573@users.noreply.github.com>
2020-12-30 22:06:50 +0200

    Fixed the sum generator

--- gen.js
+++ gen.js
@@ -1,38 +1,36 @@
 const getRandInt = (limit = 10) => Math.floor(Math.random() * limit);
 
-const getSum = (...arr) => arr.reduce((total, num) => { return total + num }, 0);
-
-let num1 = getRandInt();
-let num2 = getRandInt();
-let answer = getSum(num1, num2);
+const getTotal = (...arr) => arr.reduce((total, num) => { return total + num }, 0);
 
 const checkAnswer = (a,r) => parseInt(a) === r;
 
-console.log(num1);
-console.log(num2);
-console.log(answer);
+let num1 = getRandInt();
+let num2 = getRandInt();
+let sum = getTotal(num1,num2);
 
-(function () {
-    document.getElementById("num1").innerText = num1;
-    document.getElementById("num2").innerText = num2;
-    //document.getElementById("answer").innerText = answer;
-}());
+let elemNum1 = document.getElementById("num1");
+let elemNum2 = document.getElementById("num2");
+let elemAnswer = document.getElementById("validateAnswer");
+
+function updateElements() {
+    elemNum1.innerText = num1;
+    elemNum2.innerText = num2;
+    elemAnswer.value = 0;
+}
 
 
+updateElements(num1,num2);
 
 function checker() {
-    const result = document.getElementById("validateAnswer");
+    answer = elemAnswer.valueAsNumber;
 
-    console.log("Your answer is " + result.value);
-
-    if (checkAnswer(result.value,answer)) {
+    if (checkAnswer(answer,sum)) {
         document.getElementById("status").innerText = "Correct, try the next one ...";
-        let num1 = getRandInt();
-        let num2 = getRandInt();
-        let answer = getSum(num1,num2);
-        document.getElementById("num1").innerText = num1;
-        document.getElementById("num2").innerText = num2;
-        document.getElementById("validateAnswer").innerText = parseInt(0);
+        num1 = getRandInt();
+        num2 = getRandInt();
+        sum = getTotal(num1,num2);
+        updateElements();
+        // elemAnswer.innerText = 0;
     } else {
         document.getElementById("status").innerText = "Oops, try again ...";
     }
